334f127a26d01932882f2e1e9e461ad317782d0c
Elliot Waxman <elliot@climatedao.xyz>
2023-11-18 16:50:11 -0500

    Adding basic Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Portfolio from './Portfolio'; // This should match the filename of your Portfolio component
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SUCK MY DICK <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+          {/* If you have other routes, define them here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Portfolio.js
@@ -0,0 +1,14 @@
+// src/Portfolio.js
+
+import React from 'react';
+import './Portfolio.css'; // If you have specific styles for your portfolio
+
+function Portfolio() {
+  return (
+    <div className="portfolio">
+      {/* Your portfolio content goes here */}
+    </div>
+  );
+}
+
+export default Portfolio;
