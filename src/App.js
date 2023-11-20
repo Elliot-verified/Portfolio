ed029ec365c4cef0774165fa8f5c977cddab8fd1
Elliot Waxman <elliot@climatedao.xyz>
2023-11-20 14:28:10 -0500

    adding writing section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './Sidebar.css';
 import MainLayout from './MainLayout';
 import Contact from './Contact';
 import HomePage from './HomePage';
+import Writing from './Writing';
 import BookShelf from './BookShelf';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -15,6 +16,7 @@ const App = () => (
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path='/BookShelf' element= {<BookShelf />}/>
+        <Route path='/Writing' element= {<Writing />}/>
         <Route path='/Contact' element= {<Contact />}/>
         {/* other routes */}
       </Routes>
---
+++ src/Writing.js
@@ -0,0 +1,13 @@
+// Writing.js
+const Writing = () => (
+    <div>
+        <h2>Text here</h2>
+        <iframe src="https://elliotwaxman.substack.com/embed" width="480" height="150" frameborder="0" ></iframe>
+    </div>
+  );
+  
+  export default Writing;
+  
+
+
+
